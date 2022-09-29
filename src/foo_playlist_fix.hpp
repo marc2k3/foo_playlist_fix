71bf3a9be5a6c05581a958bd7decfe81ad6836e0
marc2k3 <marc2003@live.co.uk>
2022-09-29 03:35:38 +0100

    Tidy up

--- src/foo_playlist_fix.hpp
+++ src/foo_playlist_fix.hpp
@@ -18,56 +18,7 @@ struct ListItem
 
 using ListItems = std::vector<ListItem>;
 
-static ListItems get_patterns()
-{
-	ListItems items;
-
-	const std::string str = fb2k::configStore::get()->getConfigString("foo_playlist_fix.patterns", "[]")->c_str();
-	json patterns = json::parse(str, nullptr, false);
-	if (patterns.is_array())
-	{
-		for (auto&& pattern : patterns)
-		{
-			if (pattern.is_array() && pattern.size() == 2 && pattern[0].is_string() && pattern[1].is_string())
-			{
-				const std::string name = pattern[0].get<std::string>();
-				const std::string value = pattern[1].get<std::string>();
-				if (!name.empty() && !value.empty())
-				{
-					items.emplace_back(ListItem(name, value));
-				}
-			}
-		}
-	}
-
-	if (items.empty())
-	{
-		items.emplace_back(ListItem("Default", component_default_pattern));
-	}
-	return items;
-}
-
-static std::string get_pattern_name()
-{
-	return fb2k::configStore::get()->getConfigString("foo_playlist_fix.pattern_name", "Default")->c_str();
-}
-
-static void set_patterns(const ListItems& patterns)
-{
-	json j = json::array();
-	for (const auto& [name, pattern] : patterns)
-	{
-		if (!name.empty() && !pattern.empty())
-		{
-			json a = json::array({ name, pattern });
-			j.emplace_back(a);
-		}
-	}
-
-	fb2k::configStore::get()->setConfigString("foo_playlist_fix.patterns", j.dump().c_str());
-}
-
-static void set_pattern_name(const std::string& name)
-{
-	fb2k::configStore::get()->setConfigString("foo_playlist_fix.pattern_name", name.c_str());
-}
+ListItems get_patterns();
+std::string get_pattern_name();
+void set_patterns(const ListItems& patterns);
+void set_pattern_name(const std::string& name);
